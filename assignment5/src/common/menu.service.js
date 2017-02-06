b06cac8210fc98b2479e569b22f8d371cefa823f
Carlos Castillo Alarcón <castillo.st@gmail.com>
2017-02-06 18:28:32 +0100

    Added menu item endpoint to MenuService

--- assignment5/src/common/menu.service.js
+++ assignment5/src/common/menu.service.js
@@ -1,34 +1,54 @@
 (function () {
-"use strict";
+  'use strict';
 
-angular.module('common')
-.service('MenuService', MenuService);
-
-
-MenuService.$inject = ['$http', 'ApiPath'];
-function MenuService($http, ApiPath) {
-  var service = this;
-
-  service.getCategories = function () {
-    return $http.get(ApiPath + '/categories.json').then(function (response) {
-      return response.data;
-    });
-  };
+  angular.module('common')
+    .service('MenuService', MenuService);
 
 
-  service.getMenuItems = function (category) {
-    var config = {};
-    if (category) {
-      config.params = {'category': category};
-    }
+  MenuService.$inject = ['$log', '$interpolate', '$q', '$http', 'ApiPath'];
+  function MenuService($log, $interpolate, $q, $http, ApiPath) {
+    var service = this;
 
-    return $http.get(ApiPath + '/menu_items.json', config).then(function (response) {
-      return response.data;
-    });
-  };
-
-}
+    service.getCategories = function () {
+      return $http.get(ApiPath + '/categories.json').then(function (response) {
+        return response.data;
+      });
+    };
 
 
+    service.getMenuItems = function (category) {
+      var config = {};
+      if (category) {
+        config.params = { 'category': category };
+      }
 
+      return $http.get(ApiPath + '/menu_items.json', config).then(function (response) {
+        return response.data;
+      });
+    };
+
+    service.getMenuItemByShortName = function (shortName) {
+      var getUrlFn = $interpolate('{{apiPath}}/menu_items/{{shortName}}.json');
+      var url = getUrlFn({
+        apiPath: ApiPath,
+        shortName: shortName
+      });
+
+      return $http.get(url).then(function (response) {
+        return response.data;
+      })
+        .catch(function (response) {
+          if (response instanceof Error) {
+            $log.error('unexpected error while requesting menu item %s', shortName, response);
+            return $q.reject('Unexpected Error. Please try again later');
+          }
+          if (response.status === 404) {
+            return $q.reject('Menu item does not exists: ' + shortName);
+          }
+
+          return $q.reject(response.status + ' - ' + response.statusText);
+        });
+    };
+
+  }
 })();
